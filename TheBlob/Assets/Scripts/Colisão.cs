f9b6112432d8f5ef218a5f20a91eb092bac42362
matheuspb95 <matheuspb95@gmail.com>
2015-03-30 10:22:31 -0400

    Spawn aleatorio de inimigos

---
+++ TheBlob/Assets/Scripts/Colisão.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Colisão : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	 
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D cool){
+		if (cool.gameObject.tag == "BlobBullet") {
+			gameObject.Recycle();
+			//Debug.Log("Destroy");
+				
+		
+		}
+
+
+		}
+
+}
