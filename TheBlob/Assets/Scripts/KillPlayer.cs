6f21eff3c73c4d1e0f99d23a99bd9093c4ad1812
soluclea <lucas.alsilva01@gmail.com>
2015-04-02 02:24:43 -0400

    Moviments

---
+++ TheBlob/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlayer : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Die();
+	}
+	
+	void Die()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	//	void OnCollisionEnter2D(Collision2D cool){
+	//if (cool.gameObject.tag == "Enemy") {
+		//Application.LoadLevel("teste");
+		
+		
+	}
