b571efe46636aa71f188c09b622a3fe8a5076e45
soluclea <lucas.alsilva01@gmail.com>
2015-04-02 02:52:00 -0400

    123

---
+++ TheBlob/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PowerUps : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/*void OnCollisionEnter2D(Collision2D cool){
+		if (cool.gameObject.tag == "FireRate") {
+			shoot.FireRate= shoot.FireRate -0.1;
+	}
+}*/
+	}
