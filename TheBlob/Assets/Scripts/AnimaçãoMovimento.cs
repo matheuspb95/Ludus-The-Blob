599545a1a0000c2b4303e84523dafc76627f7329
soluclea <lucas.alsilva01@gmail.com>
2015-04-09 14:09:59 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	TheBlob/Assets/AnimationSprites/Le gosmita piscando_0.controller
    	TheBlob/Assets/AnimationSprites/LegosmitaPiscando.anim
    	TheBlob/Assets/AnimationSprites/MovimentoCima.anim
    	TheBlob/Assets/AnimationSprites/MovimentoDireita.anim
    	TheBlob/Assets/AnimationSprites/MovimentoEsquerda.anim
    	TheBlob/Assets/Scenes/teste.unity
    	TheBlob/Assets/Scripts/AnimaçãoMovimento.cs
    	TheBlob/LeGosmita.apk
    	TheBlob/Library/CurrentLayout.dwlt
    	TheBlob/Library/CurrentMaximizeLayout.dwlt
    	TheBlob/Library/InspectorExpandedItems.asset
    	TheBlob/Library/ScriptAssemblies/Assembly-CSharp.dll
    	TheBlob/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	TheBlob/Library/assetDatabase3
    	TheBlob/Library/expandedItems
    	TheBlob/Library/guidmapper
    	TheBlob/Library/metadata/03/03f3a3231c967774a8ebfe81fe58ea6e
    	TheBlob/Library/metadata/16/1690c69bee0bbb447aed507096681bea
    	TheBlob/Library/metadata/26/2695f8ab733da9d4cbaa43e8754d2bd8
    	TheBlob/Library/metadata/5a/5a199acb274ad9d45b91f337e6da9e33
    	TheBlob/Library/metadata/c2/c299c02548a7fe74d9eed1380b260376
    	TheBlob/Library/metadata/cd/cdd8fea9ef4e1914a9a5ff316c0dd20a
    	TheBlob/Library/metadata/dc/dc4a2c71ca76863418f4a861786312ee
    	TheBlob/Library/metadata/e8/e83b3d8218e7b3d459adecdeb69b5ef6
    	TheBlob/Library/metadata/f1/f1fafb1db2eae3e41b32ce97a2d5e893
    	TheBlob/TheBlob-csharp.sln
    	TheBlob/TheBlob.sln
    	TheBlob/TheBlob.userprefs
    
    Remastered

--- TheBlob/Assets/Scripts/AnimaçãoMovimento.cs
+++ TheBlob/Assets/Scripts/AnimaçãoMovimento.cs
@@ -6,6 +6,39 @@ public class AnimaçãoMovimento : MonoBehaviour {
 	public Animator Gosma;
 	// Use this for initialization
 
+<<<<<<< HEAD
+void Start () {
+		Gosma = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+void Update () {
+			
+	}
+
+  public void Animaçao(Vector3 Direçao){
+
+		Vector3 targetDir = Direçao;
+
+		float angle = Mathf.Atan2 (targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+		//Debug.Log (angle);
+			if((angle >45) && angle <135){
+				Gosma.SetTrigger ("MoveCima");
+				transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			}
+			if((angle>315)&&(angle<360)||(angle>0)&&(angle<45)){
+				Gosma.SetTrigger ("MoveDireita");
+				transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			}
+			if((angle>135)&&(angle<225)){
+				Gosma.SetTrigger ("MoveEsquerda");
+				transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			}
+			if((angle>225)&&(angle<315)){
+				Gosma.SetTrigger ("MoveBaixo");
+				transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			}
+=======
   public void Animaçao(Vector3 Direçao){
 
 		Vector3 targetDir = Direçao;
@@ -47,11 +80,6 @@ public class AnimaçãoMovimento : MonoBehaviour {
 
 	void Start () {
 		Gosma = GetComponent<Animator> ();
+>>>>>>> origin/master
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-
-}
 }
