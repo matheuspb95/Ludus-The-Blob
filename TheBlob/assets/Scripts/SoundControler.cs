42a71475fe7c6fd6b71d8788a54e0c786234bd44
YonHoshihara <hoshiharyon@gmail.com>
2015-04-15 17:40:48 -0400

    Sounds GameOver

---
+++ TheBlob/assets/Scripts/SoundControler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundControler : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	//function Awake () {
+
+		//if(Application.loadedLevelName  == "Menu_Planets"){
+
+			//DontDestroyOnLoad (transform.gameObject);
+		//}
+
+
+
+	//}
+
+
+
+
+
+	void Start () {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Application.loadedLevelName  == "stage1"){
+			
+			Destroy (transform.gameObject);
+		}
+	
+	}
+}
