65a7e868ae28f8117111ee03c4752f12fe424174
Kazuo <hoshiharyon@gmail.com>
2015-05-13 19:17:45 -0400

    Revert "Revert "Barra de fome""
    This reverts commit 17963bb157547cbfc7dee11f0329c6abfc7e4aca.

---
+++ TheBlob/assets/Scripts/HungryColision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HungryColision : MonoBehaviour {
+
+	public float hitpoints=0;
+	public float tamMax;
+	public float addFood;
+	void Start () {
+
+	}
+	void Update () {
+			}
+	void OnTriggerEnter2D(Collider2D cool){
+		if (cool.gameObject.tag == "Food") {
+						hitpoints += addFood;
+				}
+		if(hitpoints>= tamMax){
+		 Application.LoadLevel("victory");
+
+
+			}
+		
+		}
+	
+}
---
+++ TheBlob/assets/Scripts/LifeBarController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeBarController : MonoBehaviour {
+	float maxHp;
+	float currentHp;
+	GameObject redLifeBar;
+	public float hpLerpSpeed;
+	Vector3 target;
+	HungryColision scriptRef;
+
+	void Start () {
+		target = transform.localScale;
+		scriptRef = GameObject.FindGameObjectWithTag ("Player").GetComponent<HungryColision> ();
+		maxHp = scriptRef.tamMax;
+	}
+
+	void Update () {
+		AdjustGreenBar ();
+	}
+
+	void AdjustGreenBar(){
+		target.x = currentHp;
+			currentHp = scriptRef.hitpoints / maxHp;
+		if(transform.localScale.x < 0.05f){
+			transform.localScale = new Vector3(0,1,1);
+		}
+		transform.localScale = Vector3.Lerp (transform.localScale, target, Time.deltaTime * hpLerpSpeed);
+	}
+}
