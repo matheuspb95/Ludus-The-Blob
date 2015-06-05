873ae5f38e060a49da7cc6c0c816d6b4ef286d67
Lucas <lucas.alsilva01@gmail.com>
2015-06-04 21:11:55 -0400

    Optimized  and cleaned Project/Smaller apk

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Food : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D cool){
+		if (cool.gameObject.tag == "Player") {
+			StartCoroutine(Example());
+
+		}
+	}
+
+	IEnumerator Example() {
+		yield return new WaitForSeconds (0.1f);
+		    gameObject.Recycle();
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/HungryColision.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HungryColision : MonoBehaviour {
+	public Animator AnimRef;
+	public float hitpoints=0;
+	public float addFood;
+    public float max;
+
+	public float GetMax(){
+		return this.max;
+		}
+	
+
+	void Start () {
+	
+	}
+	void Update () {
+			}
+	 
+
+	
+	IEnumerator Example() {
+				yield return new WaitForSeconds (1);
+				Application.LoadLevel("victory");
+		}
+
+	void OnTriggerEnter2D(Collider2D cool){
+		if (cool.gameObject.tag == "Food") {
+						hitpoints += addFood;
+			AnimRef.SetTrigger("Eat");
+				}
+		if(hitpoints>= max ){
+		
+			StartCoroutine(Example());
+		    
+
+
+
+			}
+		
+		}
+	
+}
