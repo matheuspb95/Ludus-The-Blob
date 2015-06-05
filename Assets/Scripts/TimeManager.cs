9347ea102539a9d845522c234ec4e2d8c4a24809
Lucas <lucas.alsilva01@gmail.com>
2015-06-05 12:22:06 -0400

    Optimized and cleaned Project/ Smaller APK(Lucas)

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour {
+	Text timetext;                      // Reference to the Text component.
+	public int VictoryTime;
+	void Awake ()
+	{
+	}
+	void Update ()
+	{
+		if(Time.timeSinceLevelLoad>=VictoryTime){
+			PlayerPrefs.SetInt("Actual Score", GameObject.Find("Manager").GetComponent<ScoreManager>().GetScore());
+			Application.LoadLevel("victory");
+		}
+	}
+}
---
+++ Assets/Scripts/shoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+	public float BulletVelocity;
+	public float bulletOffset;
+	public int angleVariation;
+	public GameObject BulletPrefab;
+	private float nextFire;
+	public float FirePerSecond;
+	public void Shoot(Vector3 position){
+		if(nextFire < Time.fixedTime){
+			//Debug.Log ("shoot1");
+			nextFire = Time.fixedTime + Mathf.Pow(FirePerSecond, -1);
+			Vector2 direction = position - transform.position;
+			GameObject NewBullet = BulletPrefab.Spawn (transform.position);
+			NewBullet.rigidbody2D.velocity = direction.normalized * BulletVelocity;
+		}
+	}
+}
