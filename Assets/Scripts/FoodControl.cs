5a73a0007bf16439ae36a4bf4012eb47c94faf1b
matheuspb95 <matheuspb95@gmail.com>
2015-06-11 14:56:49 -0700

    Novo projeto

--- Assets/Scripts/FoodControl.cs
+++ Assets/Scripts/FoodControl.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class FoodControl : MonoBehaviour {
 	public float chance;
 	public GameObject Food;
+	public float Max;
+	public float AddFood;
 	public void CanISpawn(Vector3 position){
 		if (Random.Range (0.0f, 100f) >= this.chance) {
 			Instantiate(Food,position,transform.rotation);
@@ -11,7 +13,6 @@ public class FoodControl : MonoBehaviour {
 	}
 	// Use this for initialization
 	void Start () {
-
 	}
 	// Update is called once per frame
 	void Update () {
