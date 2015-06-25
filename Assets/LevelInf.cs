db06a061ef4038de5992954aab75cee42d5e862f
Lucas <lucas.alsilva01@gmail.com>
2015-06-25 15:45:34 -0400

    5 levels, 5 enemies

---
+++ Assets/LevelInf.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelInf : MonoBehaviour {
+	public string NextLevel;
+	private string CurrentLevel;
+	// Use this for initialization
+	void Start () {
+		this.CurrentLevel = Application.loadedLevelName;
+		GameObject.Find("LevelControl").GetComponent("LevelScript").LastLevelPlayed=CurrentLevel;
+		GameObject.Find("LevelControl").GetComponent("LevelScript").SetNextLevel(NextLevel);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/LevelScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelScript : MonoBehaviour {
+	string LastLevelPlayed,NextLevel;
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+//	public string GetlastLevelPlayed(){
+//		return this.LastLevelPlayed;
+//	}
+//	public void SetlastLevelPlayed(string LastLevel){
+//		this.LastLevelPlayed=LastLevel;
+//	}
+//	public string GetNextLevel(){
+//		return this.NextLevel;
+//	}
+//	public void SetNextLevel(string NextLevel){
+//		this.NextLevel=NextLevel;
+//	}
+}
